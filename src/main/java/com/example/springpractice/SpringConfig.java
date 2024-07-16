2c7d922f6b7c47aff94de2adc998cb9c0c7d06ac
DokyungYou <willow74rr@gmail.com>
2024-07-16 17:01:55 +0900

    회원 관리 예제 (db접근 연습) - 순수 JDBC

--- src/main/java/com/example/springpractice/SpringConfig.java
+++ src/main/java/com/example/springpractice/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example.springpractice;
 
+import com.example.springpractice.repository.JdbcMemberRepository;
 import com.example.springpractice.repository.MemberRepository;
 import com.example.springpractice.repository.MemoryMemberRepository;
 import com.example.springpractice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource); // 구현체만 바꿔끼면 된다. (서비스 로직은 변경 x, OCP원칙)
     }
 }
