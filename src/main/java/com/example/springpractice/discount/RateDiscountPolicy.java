ea9682723b57f705372fc2fe2ee9c0f2d5fda718
DokyungYou <willow74rr@gmail.com>
2024-07-19 14:52:04 +0900

    정률할인정책 - 테스트

---
+++ src/main/java/com/example/springpractice/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.example.springpractice.discount;
+
+import com.example.springpractice.member.Grade;
+import com.example.springpractice.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 5;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100;
+        }
+        return 0;
+    }
+}
