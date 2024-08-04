c8338c155d57b74a3e116aee8d1e3c588f686257
DokyungYou <willow74rr@gmail.com>
2024-08-05 00:40:14 +0900

    RedirectAttributes

--- src/main/java/com/example/springpractice/web/basic/BasicItemController.java
+++ src/main/java/com/example/springpractice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -105,15 +106,24 @@ public class BasicItemController {
 //        return "basic/item";
 //    }
 
-    @PostMapping("/add")
-    public String addItemV5(Item item){
+//    @PostMapping("/add")
+//    public String addItemV5(Item item){
+//
+//        Item savedItem = itemRepository.save(item);
+//        //model.addAttribute("item", savedItem);
+//
+//        return "redirect:/basic/items/" + item.getId();
+//    }
 
-        log.debug("item.name = {}", item.getName());
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
 
         Item savedItem = itemRepository.save(item);
-        //model.addAttribute("item", savedItem);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        // http://localhost:8080/basic/items/{itemId}?status=true
 
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
