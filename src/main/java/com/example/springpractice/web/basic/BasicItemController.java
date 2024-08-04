f2d9546273b3082c2ced8e1905bc72cf8d6cae26
DokyungYou <willow74rr@gmail.com>
2024-08-04 21:49:21 +0900

    상품 목록

---
+++ src/main/java/com/example/springpractice/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package com.example.springpractice.web.basic;
+
+import com.example.springpractice.domain.item.Item;
+import com.example.springpractice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final 이 붙은 멤버변수만 사용해서 생성자를 자동으로 만들어줌
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터
+     */
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("상품1",10_000,2));
+        itemRepository.save(new Item("상품2",100_000,1));
+    }
+}
