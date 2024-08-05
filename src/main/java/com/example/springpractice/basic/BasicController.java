6c3a5dbf1dc36449fb44c1cc3bc4fa80c9322863
DokyungYou <willow74rr@gmail.com>
2024-08-05 23:02:55 +0900

    주석

--- src/main/java/com/example/springpractice/basic/BasicController.java
+++ src/main/java/com/example/springpractice/basic/BasicController.java
@@ -112,6 +112,12 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
 
     // 실습용
     private void addUsers(Model model){
