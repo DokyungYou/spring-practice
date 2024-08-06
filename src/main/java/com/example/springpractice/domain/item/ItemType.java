8f36df2eea85a2b51e37e733f823e29f8e49c31b
DokyungYou <willow74rr@gmail.com>
2024-08-06 15:16:57 +0900

    요구사항 추가세팅
    - 판매여부 (checkbox)
    - 등록지역 (checkbox 다중선택)
    - 상품 종류 (radio)
    - 배송 방식 (select)

---
+++ src/main/java/com/example/springpractice/domain/item/ItemType.java
@@ -0,0 +1,13 @@
+package com.example.springpractice.domain.item;
+
+public enum ItemType {
+    
+    BOOK("도서"),
+    FOOD("식품"),
+    ETC("기타");
+    private final String description;
+
+    ItemType(String description){ // enum의 생성자 접근제어자는 private
+        this.description = description;
+    }
+}
