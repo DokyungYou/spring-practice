4f4a02461a947fc23a74f8ad6338a2bfea18dfc9
DokyungYou <willow74rr@gmail.com>
2024-08-07 14:48:18 +0900

    BindingResult

--- src/main/java/com/example/springpractice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/com/example/springpractice/web/validation/ValidationItemControllerV2.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,20 +46,20 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
-
-        // 검증 오류 결과를 보관
-        Map<String, String> errors = new HashMap<>();
+    public String addItemV1(@ModelAttribute Item item,
+                          BindingResult bindingResult, // 위의 객체에 바인딩 된 결과를 담는용 (바인딩이 되는 파마리터 바로 다음의 위치에서 사용해야한다.)
+                          RedirectAttributes redirectAttributes,
+                          Model model) {
 
         // 검증 로직
         if(!StringUtils.hasText(item.getItemName())){
-            errors.put("itemName", "상품 이름은 필수입니다.");
+            bindingResult.addError(new FieldError("item","itemName", "상품 이름은 필수입니다."));
         }
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1_000_000){
-            errors.put("price", " 가격은 1,000 ~ 1,000,000원까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","price",  " 가격은 1,000 ~ 1,000,000원까지 허용합니다."));
         }
         if(item.getQuantity() == null || item.getQuantity() > 9999){
-            errors.put("quantity","수량은 최대 9,999개 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","quantity",  "수량은 최대 9,999개 까지 허용합니다."));
         }
         
         // 특정 필드가 아닌 복합 룰 검증
@@ -64,14 +67,15 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if(resultPrice < 10000){
                 // 특정 필드가 아니어서 임의로 글로벌 오류로 지칭
-                errors.put("globalError","가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값 : " + resultPrice + "원");
+                bindingResult.addError(new ObjectError("item",
+                                "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값 :"  + resultPrice + "원"));
             }
         }
 
         // 검증 실패 시 다시 입력 form 으로 이동
-        if(!errors.isEmpty()){ // 실무에서 부정의 부정식으로 쓰면 직관적이지 못하기 때문에 웬만하면, hasError() 이런식으로 하는걸 추천 (혹은 제공해줌)
-            log.info("errors = {}", errors);
-            model.addAttribute("errors", errors);
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            // BindingResult 는 자동으로 view에 넘어가기때문에 model에 따로 직접 넣어줄 필요가 없다.
 
             // @ModelAttribute 로 값을 받아왔기때문에, 해당 form 으로 돌아갈 때 다시 가지고 감
             // (addForm 에선 빈 객체를 받아서 랜더링 하게끔 해놔서 가능, @GetMapping("/add") 참고)
