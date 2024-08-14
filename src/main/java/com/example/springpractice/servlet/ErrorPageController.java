4bf50e09d68501f3641219ebdd71bf47fa7e2c31
DokyungYou <willow74rr@gmail.com>
2024-08-14 15:53:22 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- src/main/java/com/example/springpractice/servlet/ErrorPageController.java
+++ src/main/java/com/example/springpractice/servlet/ErrorPageController.java
@@ -12,15 +12,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/error-page")
 public class ErrorPageController {
 
+    //RequestDispatcher 상수로 정의되어 있음
+    public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
+
     @GetMapping("/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response){
         log.info("errorPage 404");
+        printErrorInfo(request);
+
         return "error-page/404";
     }
 
     @GetMapping("/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response){
         log.info("errorPage 500");
+        printErrorInfo(request);
+
         return "error-page/500";
+    }
+
+    /**
+     * WAS 가 오류 페이지를 요청할 때 오류 정보를 request의 attribute 에 추가해서 넘겨주기때문에
+     * 오류페이지에서 아래와 같이 전달된 오류 정보 활용가능
+     */
+    private void printErrorInfo(HttpServletRequest request){
+        log.info("ERROR_EXCEPTION: {}",request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}",request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}",request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI: {}",request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}",request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}",request.getAttribute(ERROR_STATUS_CODE));
+
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 }
