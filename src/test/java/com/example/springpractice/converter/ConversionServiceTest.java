7bf8dbf166a14259a9ef9f136551da874d65e3b0
DokyungYou <willow74rr@gmail.com>
2024-08-15 21:50:26 +0900

    ConversionService

---
+++ src/test/java/com/example/springpractice/converter/ConversionServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.springpractice.converter;
+
+import com.example.springpractice.type.IpPort;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService(){
+        // 사용할 컨버터들을 ConversionServiceTest 에 등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        
+        // 사용
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10); // StringToIntegerConverter 작동
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+        assertThat(conversionService.convert(new IpPort("127.0.0.1", 8080), String.class)).isEqualTo("127.0.0.1:8080");
+        assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
