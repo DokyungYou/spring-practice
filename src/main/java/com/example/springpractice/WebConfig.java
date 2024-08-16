fabddc08958db500051c0d4ac601aceda6a85019
DokyungYou <willow74rr@gmail.com>
2024-08-16 17:37:05 +0900

    포맷터 적용하기

--- src/main/java/com/example/springpractice/WebConfig.java
+++ src/main/java/com/example/springpractice/WebConfig.java
@@ -4,6 +4,7 @@ import com.example.springpractice.converter.IntegerToStringConverter;
 import com.example.springpractice.converter.IpPortToStringConverter;
 import com.example.springpractice.converter.StringToIntegerConverter;
 import com.example.springpractice.converter.StringToIpPortConverter;
+import com.example.springpractice.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 우선순위 때문에 주석처리 (포멧터 < 컨버터)
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        // 숫자 -> 문자, 문자-> 숫자
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
