76df01407312febc0962b80dc93945d6e2a7f4f6
DokyungYou <willow74rr@gmail.com>
2024-08-22 08:39:45 +0900

    테스트 - 데이터 롤백
    - beforeEach, afterEach 에 트랙잭션 코드 추가

--- src/test/java/com/example/springpractice/domain/ItemRepositoryTest.java
+++ src/test/java/com/example/springpractice/domain/ItemRepositoryTest.java
@@ -5,9 +5,13 @@ import com.example.springpractice.repository.ItemSearchCondition;
 import com.example.springpractice.repository.ItemUpdateDto;
 import com.example.springpractice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -22,12 +26,26 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    //트랜잭션 매니저는 스프링부트가 자동으로 빈으로 등록해줌
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        //트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
     @AfterEach
     void afterEach() {
         // MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        //트랜잭션 롤백
+        transactionManager.rollback(status);
     }
 
     @Test
