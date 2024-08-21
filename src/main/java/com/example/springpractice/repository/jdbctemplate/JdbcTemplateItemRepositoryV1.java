7fb71f49e5ab118b4b35c8c4f36a579151049024
DokyungYou <willow74rr@gmail.com>
2024-08-21 19:52:11 +0900

    JdbcTemplate 적용1 - 기본

---
+++ src/main/java/com/example/springpractice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -0,0 +1,117 @@
+package com.example.springpractice.repository.jdbctemplate;
+
+import com.example.springpractice.domain.Item;
+import com.example.springpractice.repository.ItemRepository;
+import com.example.springpractice.repository.ItemSearchCondition;
+import com.example.springpractice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name, price, quantity) values(?,?,?)";
+
+        // jdbcTemplate 을 쓸 때 db 에서 생성해준 아이디 값을 가져오려면 KeyHolder 를 사용해야함
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            // 자동 증가 키
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, item.getItemName());
+            ps.setInt(2, item.getPrice());
+            ps.setInt(2, item.getQuantity());
+            return ps;
+
+        }, keyHolder);
+
+        // 데이터베이스에 Insert 가 완료돼야 생성된 PK ID 값 확인 가능
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+
+        return item;
+    }
+
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name=?, price=?, quantity=? where id=?";
+        jdbcTemplate.update(sql,
+                updateParam.getItemName(),
+                updateParam.getPrice(),
+                updateParam.getQuantity(),
+                itemId);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+
+        String sql = "select id, item_name, price, quantity from item where id = ?";
+        
+        try {
+            // 데이터가 없으면 예외터짐
+            Item item = jdbcTemplate.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCondition condition) {
+        String itemName = condition.getItemName();
+        Integer maxPrice = condition.getMaxPrice();
+        String sql = "select id, item_name, price, quantity from item";
+
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+
+        boolean andFlag = false;
+        List<Object> param = new ArrayList<>();
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',?,'%')";
+            param.add(itemName);
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= ?";
+            param.add(maxPrice);
+        }
+        log.info("sql={}", sql);
+        return jdbcTemplate.query(sql, itemRowMapper(), param.toArray());
+    }
+
+
+    private RowMapper<Item> itemRowMapper() {
+        return (rs, rowNum) -> {
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+            return item;
+        };
+    }
+}
