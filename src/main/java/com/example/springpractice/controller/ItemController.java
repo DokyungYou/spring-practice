d8c445eb01a28a8af4bfd329b7243ca3247afda7
DokyungYou <willow74rr@gmail.com>
2024-08-28 22:32:22 +0900

    상품 수정

--- src/main/java/com/example/springpractice/controller/ItemController.java
+++ src/main/java/com/example/springpractice/controller/ItemController.java
@@ -9,9 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,5 +46,40 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "/items/itemList";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId")Long itemId, Model model){
+        Book item = (Book) itemService.findOne(itemId);
+
+        // setter 방식은 지양
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+        form.setStockQuantity(item.getStockQuantity());
+
+        model.addAttribute("form", form);
+
+        return "/items/updateItemForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form){
+
+        // setter 방식은 지양
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+
+        book.setIsbn(form.getIsbn());
+        itemService.saveItem(book);
+
+        return "redirect:/items";
     }
 }
