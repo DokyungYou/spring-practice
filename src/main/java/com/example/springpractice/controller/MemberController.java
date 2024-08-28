d0b2b16a5bc3a5b4520fad583f2f643a63cd0ca7
DokyungYou <willow74rr@gmail.com>
2024-08-28 20:04:57 +0900

    회원 등록

---
+++ src/main/java/com/example/springpractice/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.example.springpractice.controller;
+
+import com.example.springpractice.domain.Address;
+import com.example.springpractice.domain.Member;
+import com.example.springpractice.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/members")
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid MemberForm form, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
