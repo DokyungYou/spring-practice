8e8e96ecfcba57f3d720fb6a8f44b6ca715538d8
DokyungYou <willow74rr@gmail.com>
2024-08-28 09:48:53 +0900

    상품 service & Repository 개발

---
+++ src/main/java/com/example/springpractice/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package com.example.springpractice.repository;
+
+import com.example.springpractice.domain.item.Item;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class ItemRepository {
+
+    private final EntityManager entityManager;
+
+    public void save(Item item){
+        if(item.getId() != null){ // 새로 생성한 객체
+            entityManager.persist(item);
+        } else{
+            entityManager.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return entityManager.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return entityManager.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+
+}
