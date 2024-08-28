cf657e86da2adffc56091d847cee14dce61b1a78
DokyungYou <willow74rr@gmail.com>
2024-08-28 21:49:38 +0900

    상품 등록

---
+++ src/main/java/com/example/springpractice/controller/ItemController.java
@@ -0,0 +1,42 @@
+package com.example.springpractice.controller;
+
+import com.example.springpractice.domain.item.Book;
+import com.example.springpractice.repository.ItemRepository;
+import com.example.springpractice.service.ItemService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/items")
+@Controller
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "/items/createItemForm";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid BookForm form){
+        
+        // setter 방식은 지양
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(book.getIsbn());
+        book.setStockQuantity(form.getStockQuantity());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+}
