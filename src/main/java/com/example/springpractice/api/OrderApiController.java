ef543d6f86d04587ed6a0b9fc4b92f58cc18e49d
DokyungYou <willow74rr@gmail.com>
2024-09-05 01:38:54 +0900

    주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/com/example/springpractice/api/OrderApiController.java
+++ src/main/java/com/example/springpractice/api/OrderApiController.java
@@ -1,15 +1,20 @@
 package com.example.springpractice.api;
 
 import com.example.springpractice.OrderSearch;
+import com.example.springpractice.domain.Address;
 import com.example.springpractice.domain.Order;
 import com.example.springpractice.domain.OrderItem;
+import com.example.springpractice.domain.enums.OrderStatus;
 import com.example.springpractice.repository.order.OrderRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -32,8 +37,10 @@ public class OrderApiController {
      *  from delivery d1_0 where d1_0.delivery_id=?
      *  from orders o1_0 where o1_0.delivery_id=?
      *  from order_item oi1_0 where oi1_0.order_id=?
+     *  
+     *  orderItems 루프 (현재 실습 초기화 데이터가 Order 하나당 OrderItem 2개 들어있는 상황)
      *  from item i1_0 where i1_0.item_id=?
-     *  from item i1_0 where i1_0.item_id=? // 왜 한번 더 나오지
+     *  from item i1_0 where i1_0.item_id=? 
      */
     @GetMapping("/v1/orders")
     public List<Order> ordersV1(){
@@ -50,5 +57,63 @@ public class OrderApiController {
         }
 
         return orders;
+    }
+
+    /**
+     *SQL 실행 수 (V1과 같음)
+     * order  1번
+     * member , address  N번 (order 조회 수 만큼)
+     * orderItem  N번  (order 조회 수 만큼)
+     * item  N번 (orderItem 조회 수 만큼)
+     */
+    @GetMapping("/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<OrderDto> collect = orders.stream()
+                //.map(order -> new OrderDto(order))
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderedAt;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+
+        // DTO 세팅하면서 Lazy 초기화됨
+        public OrderDto(Order order){
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderedAt = order.getOrderDateTime();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+
+            this.orderItems = order.getOrderItems()
+                    .stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        // DTO 세팅하면서 Lazy 초기화됨
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
