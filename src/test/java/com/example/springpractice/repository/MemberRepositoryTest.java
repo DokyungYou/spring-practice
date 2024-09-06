1f55a0e31cb0226ed401f9abf794acd9a32dd326
DokyungYou <willow74rr@gmail.com>
2024-09-06 19:31:35 +0900

    공통 인터페이스 적용

--- src/test/java/com/example/springpractice/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/springpractice/repository/MemberRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
@@ -30,4 +32,30 @@ class MemberRepositoryTest {
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장 (같은 트랜잭션 내에서 영속성 컨텍스트의 동일성 보장)
         }
+
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("멤버1");
+        Member member2 = new Member("멤버2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회
+        Member findMember1 = memberRepository.findById(member1.getId()).get();// 바로 get() 지양
+        Member findMember2 = memberRepository.findById(member2.getId()).get();// 바로 get() 지양
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> members = memberRepository.findAll();
+        assertThat(members.size()).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        assertThat(memberRepository.count()).isEqualTo(0);
+
+    }
 }
