5893ad9aab925323a3f725bfc51bff5100df0676
DokyungYou <willow74rr@gmail.com>
2024-09-08 19:18:15 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/com/example/springpractice/controller/MemberController.java
+++ src/main/java/com/example/springpractice/controller/MemberController.java
@@ -1,9 +1,15 @@
 package com.example.springpractice.controller;
 
+import com.example.springpractice.dto.MemberDto;
 import com.example.springpractice.entity.Member;
 import com.example.springpractice.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +38,48 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * Pageable 은 인터페이스, 실제는 구현체인 org.springframework.data.domain.PageRequest 객체 생성 후 인젝션
+     *
+     * ex) /members?page=0&size=3&sort=id,desc&sort=username,desc
+     * page: 현재 페이지, 0부터 시작한다.
+     * size: 한 페이지에 노출할 데이터 건수
+     * sort: 정렬 조건을 정의한다. 예) 정렬 속성,정렬 속성...(ASC | DESC), 정렬 방향을 변경하고 싶으면 sort 파라 미터 추가 ( asc 생략 가능)
+     *
+     * 디폴트 설정
+     * - 글로벌 설정
+     * - @PageableDefault
+     */
+    @GetMapping("/members1")
+    public Page<Member> list1(@PageableDefault(size = 12, sort = "username", direction = Sort.Direction.DESC)
+                                 Pageable pageable){
+        // finalAll 과 같은 기본메서드에 Pageable 을 파라미터로 넘길 수 있음
+        return memberRepository.findAll(pageable);
+    }
+
+    /**
+     * 페이징 정보가 둘 이상이면 접두사로 구분
+     * @Qualifier 에 접두사명 추가 "{접두사명}_xxx" 예제: /members?member_page=0&order_page=1
+     */
+    @GetMapping("/members2")
+    public Page<Member> list2(@Qualifier("member") Pageable memberPageable,
+                              @Qualifier("team") Pageable teamPageable){
+        // finalAll 과 같은 기본메서드에 Pageable 을 파라미터로 넘길 수 있음
+        return memberRepository.findAll(memberPageable);
+    }
+
+    @GetMapping("/members3")
+    public Page<MemberDto> list3(@PageableDefault(size = 12, sort = "username", direction = Sort.Direction.DESC)
+                              Pageable pageable){
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(MemberDto::new);
+    }
+
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("멤버1"));
+        for (int i = 1; i < 100; i++) {
+            memberRepository.save(new Member("멤버" + i, i));
+        }
     }
 }
--- src/main/java/com/example/springpractice/dto/MemberDto.java
+++ src/main/java/com/example/springpractice/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.example.springpractice.dto;
 
+import com.example.springpractice.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +15,11 @@ public class MemberDto {
     private String username;
     private String teamName;
 
+    // 의존관계에 있어 엔티티는 DTO를 의존하지 않아야함
+    // DTO 가 엔티티를 의존하는 것은 괜찮음
+    public MemberDto(Member member){
+        this.id = member.getId();
+        this.username = member.getUsername();
+        //this.teamName = member.getTeam().getName();
+    }
 }
